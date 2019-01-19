d23b3c9b66b74f7d91b5da375c3e6e4626b75ab1
visualstudio <visualstudio@122-PC29>
2019-01-18 20:52:32 -0500

    new coding

--- C0731847/Program.cs
+++ C0731847/Program.cs
@@ -10,7 +10,30 @@ namespace C0731847
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int>();
+            StudentInformation _Mary = new StudentInformation("Mary", 3.8, 20);
+            StudentInformation _John = new StudentInformation("John", 2.0, 10);
+            StudentInformation _Mark = new StudentInformation("Mark", 0.2, 3);
+            StudentInformation _Steve = new StudentInformation("Steve", 2.9, 16);
+            StudentInformation _Cindy = new StudentInformation("Cindy", 3.8, 20);
+            Console.ReadLine();
+        }
+
+    }
+        
+        class StudentInformation
+        {
+            public string Name;
+        
+            public double GPA;
+            public int Attendance;
+            public StudentInformation(string aname, double aGPA, int dAttendence)
+            {
+            Name = aname;
+               
+                GPA = aGPA;
+                Attendance = dAttendence;
+}
+
         }
     }
-}
+
